142fceddb3584a8dc6aed3ffe51ab499105866b4
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-06-04 15:16:04 +0200

    added form for adding happy thoughts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
 import { useEffect, useState } from "react";
 
+import Form from "./components/Form.jsx";
+
 const BASE_URL = "https://js-project-api-x10r.onrender.com";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [messageText, setMessageText] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleMessage = (event) => {
+    event.preventDefault();
+    fetch(`${BASE_URL}/messages`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: messageText }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Could not post happy thought");
+        return res.json();
+      })
+      .then(() => {
+        fetchMessages();
+        setMessageText("");
+        setError(null);
+      })
+      .catch((err) => setError(err.message));
+  };
 
   const fetchMessages = () => {
     setLoading(true);
@@ -60,6 +84,12 @@ const App = () => {
   return (
     <>
       <h1>Your daily dose of happy thoughts!</h1>
+      <Form
+        messageText={messageText}
+        setMessageText={setMessageText}
+        handleMessage={handleMessage}
+        error={error}
+      />
       <button onClick={handleClick}>Get messages with 5+ hearts</button>
 
       {messages.length > 0 &&
